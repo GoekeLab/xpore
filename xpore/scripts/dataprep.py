67367b4737a2d333715855ad8ff2e6c0c7339efc
Christopher Hendra <christopherhendra@gis.a-star.edu.sg>
2020-03-18 12:23:59 +0800

    add dtype arguments to preprocess method

--- xpore/scripts/dataprep.py
+++ xpore/scripts/dataprep.py
@@ -74,7 +74,8 @@ def combine(read_name,eventalign_per_read,out_paths,locks):
     eventalign_result['read_id'] = [read_name]*len(eventalign_result)
 
     features = ['read_id','transcript_id','transcriptomic_position','reference_kmer','norm_mean','norm_std','dwell_time','start_idx','end_idx']
-    # features_dtype = numpy.dtype([('read_id', 'S36'), ('transcript_id', 'S15'), ('transcriptomic_position', '<i8'), ('reference_kmer', 'S5'), ('norm_mean', '<f8'), ('start_idx', '<i8'), ('end_idx', '<i8')])
+    features_dtype = numpy.dtype([('read_id', 'S36'), ('transcript_id', 'S15'), ('transcriptomic_position', '<i8'), ('reference_kmer', 'S5'), ('norm_mean', '<f8'), ('start_idx', '<i8'),
+                                  ('end_idx', '<i8')])
     # features = ['read_id','transcript_id','transcriptomic_position','reference_kmer','norm_mean'] #original features that Ploy's using.
 
     df_events_per_read = eventalign_result[features]
@@ -86,7 +87,7 @@ def combine(read_name,eventalign_per_read,out_paths,locks):
     with locks['hdf5'], h5py.File(out_paths['hdf5'],'a') as hf:
         for tx_id,read_id in df_events_per_read.index.unique():
             df2write = df_events_per_read.loc[[tx_id,read_id],:].reset_index() 
-            events = numpy.rec.fromrecords(misc.str_encode(df2write[features]),names=features) #,dtype=features_dtype
+            events = numpy.rec.fromrecords(misc.str_encode(df2write[features]),names=features,dtype=features_dtype) #,dtype=features_dtype
 
             hf_tx = hf.require_group('%s/%s' %(tx_id,read_id))
             if 'events' in hf_tx:
@@ -101,7 +102,7 @@ def combine(read_name,eventalign_per_read,out_paths,locks):
 def prepare_for_inference(tx,read_task,kmers,out_dir,locks):
     reads = numpy.concatenate(read_task)
     
-    with locks['log'], open(os.path.join(out_dir, "prepare_for_inference.log"),'a') as f:
+    with locks['log'], open(os.path.join(out_dir, "inference_preparation.log"),'a') as f:
         f.write('%s\n' %(tx))    
 
 def parallel_prepare_for_inference(eventalign_filepath,out_dir,n_processes):
