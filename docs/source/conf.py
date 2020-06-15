e00d99286454f71ce936eebe6d6a2d762c545414
Ploy <naruemon.npr@gmail.com>
2020-06-15 05:38:12 +0000

    v0.5.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'Ploy N. Pratanwanich'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.4.4'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
