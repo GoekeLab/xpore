1b7e0649cea9d4538b8c25d06571b411aaf6afd4
ploy-np <naruemon.npr@gmail.com>
2020-07-27 04:01:46 +0000

    updated doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'Ploy N. Pratanwanich'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.5.2'
 
 
 # -- General configuration ---------------------------------------------------
