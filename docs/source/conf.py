f3c29551cdba0ee022f87d962c8d223b1e867b70
ploy-np <naruemon.npr@gmail.com>
2021-03-07 12:27:16 +0000

    version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ copyright = '2020, Ploy N. Pratanwanich'
 author = 'Ploy N. Pratanwanich'
 
 # The short X.Y version
-version = ''
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
