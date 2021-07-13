b4e9291af1ad49cf399e47ae13d57ef5e7cc780b
Yuk Kei Wan <ywan6@alumni.jh.edu>
2021-07-13 17:17:13 +0800

    add xpore-postprocessing

--- setup.py
+++ setup.py
@@ -31,7 +31,8 @@ setup(
             ],
     python_requires=">=3.8",
     entry_points={'console_scripts': ["xpore-dataprep={}.scripts.dataprep:main".format(__pkg_name__),
-                                      "xpore-diffmod={}.scripts.diffmod:main".format(__pkg_name__)]},
+                                      "xpore-diffmod={}.scripts.diffmod:main".format(__pkg_name__),
+                                      "xpore-postprocessing={}.scripts.postprocessing:main".format(__pkg_name__)]},
     classifiers=[
         # Trove classifiers
         # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
