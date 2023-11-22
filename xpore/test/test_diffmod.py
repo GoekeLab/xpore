923d4fcdf856c95e000aa21f8938b5135dd8d437
Yuk Kei Wan <ywan6@alumni.jh.edu>
2023-11-22 03:41:42 +0000

    fix

--- xpore/test/test_diffmod.py
+++ xpore/test/test_diffmod.py
@@ -19,7 +19,16 @@ from xpore.scripts import diffmod
 @pytest.fixture
 def diffmod_args():
     class DiffmodArgs:
-        config = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data/config.yml")
+        #config = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data/config.yml")
+        outfile=open('neues_config.yml','w')
+        for ln in open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "data/config.yml"),'r'):
+            if 'rep1' in ln:
+                ln=ln.split(':')
+                outfile.write(': '.join([ln[0], os.path.join(os.path.abspath(os.path.dirname(__file__)),ln[-1].strip())]))
+            else:
+                outfile.write(ln)
+        outfile.close()
+        config = os.path.join(os.getcwd(),'neues_config.yml')
         n_processes = 2
         save_models = False
         resume = False
