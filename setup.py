b5830c2c26473d510f6c68dc8fd107e11aa4de26
Yuk Kei Wan <ywan6@jhu.edu>
2023-11-24 14:51:04 +0800

    update version to 2.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name=__pkg_name__,
     license="MIT",
     description='xpore is a python package for Nanopore data analysis of differential RNA modifications.',
-    version='v2.1',
+    version='v2.1.1',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/GoekeLab/xpore',
